c74db507de804b3b3f1935f5cfa0cc6abb884f1a
Beth Ashton <Mbeth.ashton@gmail.com>
2020-01-28 16:24:08 -0500

    Further Implement Village

--- Player.rb
+++ Player.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 COIN_VALUE = {
-  :copper => 1,
-  :silver => 2,
-  :gold => 3
+  copper: 1,
+  silver: 2,
+  gold: 3
 }.freeze
 
 CARD_COST = {
-  :copper => 0,
-  :silver => 3,
-  :gold => 6,
-  :estate => 2,
-  :village => 3
+  copper: 0,
+  silver: 3,
+  gold: 6,
+  estate: 2,
+  village: 3
 }.freeze
 
 PLAY_CARD = {
-  :village => -> do 
-                add_action(2)
-                draw(1) 
-              end
-}
+  village: lambda do
+             add_action(2)
+             draw(1)
+           end
+}.freeze
 
 class Player
   attr_reader :draw_pile, :discard_pile, :hand, :actions
@@ -26,7 +28,7 @@ class Player
     @discard_pile = []
     @hand = []
     @actions = 0
-    7.times do 
+    7.times do
       add_card(:copper)
     end
     3.times do
@@ -35,6 +37,7 @@ class Player
     shuffle
     draw(5)
   end
+
   def new_turn
     @actions = 1
     display_state
@@ -44,59 +47,64 @@ class Player
     end
     draw(5) # players should have 5 cards in their hand, even when it's not their turn
   end
+
   def display_state
-    puts "hand:"
-    puts @hand
+    puts '-' * 30
+    puts 'hand:'
+    pp @hand
     puts "coins: #{coins_available}"
     puts "actions: #{@actions}"
   end
+
   def draw(num = 1)
     # remove card from draw_pile
     # add same card to hand
-    num.times do 
+    num.times do
       @hand.push(@draw_pile.shift)
     end
   end
+
   def discard(card)
     @discard_pile.push(card)
-    @hand = @hand - [card]
+    @hand -= [card]
   end
+
   def shuffle
     # add better shuffle here
     @draw_pile = @discard_pile.shuffle
     @discard_pile.clear
   end
-  def add_card(card_symbol, location = :discard)
+
+  def add_card(card_symbol, _location = :discard)
     # by default, cards are added to the discard pile, but sometimes they are added to the draw pile
     @discard_pile.push(card_symbol)
   end
+
   def add_action(num = 1)
     @actions += num
   end
+
   def play(card_symbol)
     # lookup card action in above hash ^
-    PLAY_CARD[card_symbol]
+    PLAY_CARD[card_symbol].call
+    display_state
   end
+
   def coins_available
     total = 0
     @hand.each do |card|
-      if COIN_VALUE.include?(card)
-        total += COIN_VALUE[card]
-      end
+      total += COIN_VALUE[card] if COIN_VALUE.include?(card)
     end
-    return total
+    total
   end
-  def use_action(num = 1)
+
+  def use_action(_num = 1)
     @actions -= 1
   end
+
   def buy(card_symbol)
-    if coins_available > CARD_COST[card_symbol]
-      add_card(card_symbol)
-    end
+    add_card(card_symbol) if coins_available > CARD_COST[card_symbol]
   end
 end
 
 player = Player.new
-player.new_turn
-player.buy(:village)
-player.new_turn

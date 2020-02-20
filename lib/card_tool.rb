86a6eb8ccbe962388551aa52e567e1aa79270c01
Beth Jackson <Mbeth.ashton@gmail.com>
2020-02-20 11:34:50 -0500

    adding some documentation

--- lib/card_tool.rb
+++ lib/card_tool.rb
@@ -4,13 +4,16 @@ def save(info)
   name = info[:name]
   price = info[:price]
   value = info[:value]
+end
 
-# Functions used in generating card classes and files.
+# Easy-to-use tool for creating new card classes automatically.
 module CardTool
+  # Write new file for type of card
   def self.save(file_path, name, price, aspects)
     IO.write(file_path, make_code(name, price, aspects))
   end
 
+  # create class code using <<~CODE
   def self.make_code(name, price, aspects = {})
     custom_methods =
       aspects
@@ -32,6 +35,7 @@ module CardTool
     CODE
   end
 
+  # make methods - specifically for small text on cards
   def self.make_method(aspect, value)
     <<~CODE.chomp.gsub("\n", "\n  ")
       def #{aspect}
